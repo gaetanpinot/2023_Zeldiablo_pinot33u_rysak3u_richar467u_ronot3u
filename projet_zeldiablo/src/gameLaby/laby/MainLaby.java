569211698a1efc550d655f2827963e62cd416424
Killian RD <killdutileul@gmail.com>
2023-05-30 16:29:12 +0200

    Ajout de la fonctionnalité 1.3

--- projet_zeldiablo/src/gameLaby/laby/MainLaby.java
+++ projet_zeldiablo/src/gameLaby/laby/MainLaby.java
@@ -3,17 +3,21 @@ package gameLaby.laby;
 import moteurJeu.MoteurJeu;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class MainLaby {
     public static void main(String[] args) throws IOException {
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entrer le nom d'un labyrinthe (avec l'extension) : ");
+        String nomFichier = sc.nextLine();
 
         int width = 800;
         int height = 600;
         int pFPS = 10;
 
         LabyJeu lj=new LabyJeu();
-        lj.laby=new Labyrinthe("labySimple/laby1.txt");
+        lj.laby=new Labyrinthe("labySimple/" + nomFichier);
         LabyDessin ld=new LabyDessin();
 
         MoteurJeu.setTaille(width,height);
