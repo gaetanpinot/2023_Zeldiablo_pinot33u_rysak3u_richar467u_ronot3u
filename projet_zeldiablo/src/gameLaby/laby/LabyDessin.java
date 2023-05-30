7813e168c745440fb8fc0052318c3ace48f7eecf
Killian RD <killdutileul@gmail.com>
2023-05-30 14:49:07 +0200

    Ajout de la classe LabyDessin

---
+++ projet_zeldiablo/src/gameLaby/laby/LabyDessin.java
@@ -0,0 +1,33 @@
+package gameLaby.laby;
+
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import moteurJeu.DessinJeu;
+import moteurJeu.Jeu;
+
+public class LabyDessin implements DessinJeu {
+    @Override
+    public void dessinerJeu(Jeu jeu, Canvas canvas) {
+        Labyrinthe laby = (Labyrinthe) jeu;
+
+        final GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        gc.setFill(Color.WHITE);
+        gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
+        for (int i = 0; i < laby.getLength(); i++) {
+            for (int j = 0; j < laby.getLengthY(); j++) {
+                if(laby.getMur(i, j)){
+                    gc.setFill(Color.BLACK);
+                    gc.fillRect(i, j, 20, 20);
+                }
+                
+                if(laby.pj.etrePresent(i, j)){
+                    gc.setFill(Color.RED);
+                    gc.fillOval(i, j, 10, 10);
+                }
+            }
+        }
+    }
+}
