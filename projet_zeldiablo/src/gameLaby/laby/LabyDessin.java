f14581a7c7103be174728a1054a0a42fa1407249
Killian RD <killdutileul@gmail.com>
2023-05-30 15:36:18 +0200

    Ajout du montre dans l'interface graphique de laby

--- projet_zeldiablo/src/gameLaby/laby/LabyDessin.java
+++ projet_zeldiablo/src/gameLaby/laby/LabyDessin.java
@@ -9,23 +9,28 @@ import moteurJeu.Jeu;
 public class LabyDessin implements DessinJeu {
     @Override
     public void dessinerJeu(Jeu jeu, Canvas canvas) {
-        Labyrinthe laby = ((LabyJeu)jeu).getLaby();
+        Labyrinthe laby = ((LabyJeu) jeu).getLaby();
 
         final GraphicsContext gc = canvas.getGraphicsContext2D();
 
         gc.setFill(Color.WHITE);
         gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
-        int taille=20;
+        int taille = 20;
         for (int i = 0; i < laby.getLength(); i++) {
             for (int j = 0; j < laby.getLengthY(); j++) {
-                if(laby.getMur(i, j)){
+                if (laby.getMur(i, j)) {
                     gc.setFill(Color.BLACK);
                     gc.fillRect(i * taille, j * taille, taille, taille);
                 }
-                
-                if(laby.pj.etrePresent(i, j)){
+
+                if (laby.pj.etrePresent(i, j)) {
                     gc.setFill(Color.RED);
-                    gc.fillOval(i * taille, j * taille, taille , taille);
+                    gc.fillOval(i * taille, j * taille, taille, taille);
+                }
+
+                if (laby.monstre.etrePresent(i, j)) {
+                    gc.setFill(Color.DARKGREEN);
+                    gc.fillOval(i * taille, j * taille, taille, taille);
                 }
             }
         }
