3df73c3baec68d4bd0c0c155c532df93d800df92
Killian RD <killdutileul@gmail.com>
2023-05-31 16:43:23 +0200

    Ajout de la fonctionnalité sur le passage du persoonage sur la case piege

--- projet_zeldiablo/src/gameLaby/laby/CasePiege.java
+++ projet_zeldiablo/src/gameLaby/laby/CasePiege.java
@@ -1,25 +1,31 @@
 package gameLaby.laby;
 
-public class CasePiege extends CaseDeclencheur{
+public class CasePiege extends CaseDeclencheur {
     boolean ammorce;
+    boolean passerDessus;
 
-    public CasePiege(int dx,int dy){
-        super(dx,dy);
-        ammorce=true;
+    public CasePiege(int dx, int dy) {
+        super(dx, dy);
+        ammorce = true;
     }
 
     @Override
     public void event(Personnage p) {
-        if(p.etrePresent(this.getX(),this.getY())){
-            if(ammorce){
+        if (p.etrePresent(this.getX(), this.getY())) {
+            if (ammorce) {
                 System.out.println("boom");
-                ammorce=false;
+                ammorce = false;
                 p.ajouterVie(-10);
+                passerDessus = true;
             }
-        }else {
-            ammorce=true;
+        } else {
+            ammorce = true;
         }
     }
+
+     public boolean getPasserDessus() {
+        return passerDessus;
+     }
 
 
 }
