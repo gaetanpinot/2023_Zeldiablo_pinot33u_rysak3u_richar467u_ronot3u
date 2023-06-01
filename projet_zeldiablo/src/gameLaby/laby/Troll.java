110f25ce24aba243580b3113b5f95f4e59ad470f
Martin Ronot <ronot.martin@gmail.com>
2023-06-01 17:35:33 +0200

    Commentaires labyJeu

--- projet_zeldiablo/src/gameLaby/laby/Troll.java
+++ projet_zeldiablo/src/gameLaby/laby/Troll.java
@@ -5,6 +5,5 @@ public class Troll extends Monstre implements Intelligence{
     String intelligence;
     public Troll(int dx, int dy, int vie) {
         super(dx,dy,vie);
-        intelligence=Intelligence.intelligence[(int)(Math.floor(Math.random()*Intelligence.intelligence.length))];
     }
 }
