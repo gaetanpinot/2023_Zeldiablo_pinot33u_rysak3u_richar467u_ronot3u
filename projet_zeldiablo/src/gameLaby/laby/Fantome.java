23e7cb038512078661c92458d54cf66bd6d61078
Hugo Rysak <hugo.rysak4@etu.unniv-lorraine.fr>
2023-06-05 09:33:38 +0200

    ajout test

--- projet_zeldiablo/src/gameLaby/laby/Fantome.java
+++ projet_zeldiablo/src/gameLaby/laby/Fantome.java
@@ -10,6 +10,10 @@ public class Fantome extends Monstre{
      */
     public Fantome(int dx, int dy, int vie) {
         super(dx, dy, vie);
+        String[] s={Intelligence.NULLE,Intelligence.FAIBLE,Intelligence.MOYENNE};
+        int r=(int)Math.floor(Math.random()*3);
+        this.setIntelligence(s[r]);
+
     }
     public Fantome(int dx, int dy, int vie,String i) {
         super(dx,dy,vie,i);
